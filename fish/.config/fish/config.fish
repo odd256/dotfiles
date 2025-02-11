0efcc7ec8f090c4e7372e3de55e06bbf8fe33357
odd <1211350624@qq.com>
2025-02-11 16:33:56 +0800

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 set -g fish_greeting
 
+# starship
 if status is-interactive
     starship init fish | source
 end
@@ -20,3 +21,13 @@ abbr .5 'cd ../../../../..'
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
+
+# yazi->y
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
